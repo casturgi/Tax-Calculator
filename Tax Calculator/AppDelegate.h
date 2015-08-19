729dbf2845464e83370ed3932343a1c594b5d4bd
Cory Sturgis-Casturgi <casturgi@gmail.com>
2015-08-18 19:36:45 -0500

    Initial Commit

---
+++ Tax Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tax Calculator
+//
+//  Created by cory Sturgis on 8/18/15.
+//  Copyright (c) 2015 CorySturgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tax Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tax Calculator
+//
+//  Created by cory Sturgis on 8/18/15.
+//  Copyright (c) 2015 CorySturgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
